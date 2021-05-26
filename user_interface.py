99b834bc3d884907511351007ba88b5b674b339a
unknown <mrchrisfarrell@yahoo.com>
2021-05-26 12:41:50 -0400

    added assign_contestant_info in UI

--- user_interface.py
+++ user_interface.py
@@ -1,2 +1,12 @@
+from contestant import Contestant
+
+
 def display_message(message):
     print(message)
+
+
+def assign_contestant_info(last_used_registration_num=1):
+    first_name = input("Enter first name")
+    last_name = input("Enter last name")
+    email = input("Enter email address")
+    return [first_name, last_name, email, last_used_registration_num + 1]
