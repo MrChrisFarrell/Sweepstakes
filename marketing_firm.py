4c947deadc3a2afd197e56a22b7a3d388df88dc2
unknown <mrchrisfarrell@yahoo.com>
2021-05-26 14:57:05 -0400

    renamed queue.py to fix bug. created marketingfirm class

---
+++ marketing_firm.py
@@ -0,0 +1,21 @@
+from user_interface import *
+from sweepstake import Sweepstake
+from contestant import Contestant
+
+
+class MarketingFirm:
+    def __init__(self, manager):
+        self.manager = manager
+
+    def create_sweepstakes(self):
+        sweepstakes_name = get_input("Name the sweepstakes - ")
+        sweepstakes = Sweepstake(sweepstakes_name)
+        add_contestant = verify("Do you want to add a contestant? ")
+        contestants_added = 0
+        while add_contestant:
+            contestant_info = assign_contestant_info(contestants_added)
+            contestant = Contestant(contestant_info[0], contestant_info[1], contestant_info[2], contestant_info[3])
+            sweepstakes.register_contestant(contestant)
+            contestants_added += 1
+            add_contestant = verify("Do you want to add another contestant? ")
+        self.manager.insert_sweepstakes(sweepstakes)
--- user_interface.py
+++ user_interface.py
@@ -13,3 +13,16 @@ def assign_contestant_info(last_used_registration_num=1):
 def print_list_iterate(supplied_list):
     for thing in supplied_list:
         print(thing)
+
+
+def get_input(message):
+    result = input(message)
+    return result
+
+
+def verify(message):
+    answer = input(message).lower()
+    if answer == 'yes':
+        return True
+    else:
+        return False
