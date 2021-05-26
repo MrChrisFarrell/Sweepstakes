699af3168696c7d95c134222c6e29e6ed4db4f7a
unknown <mrchrisfarrell@yahoo.com>
2021-05-26 14:05:18 -0400

    added pick winner and print cont info methods to sweepstakes

--- sweepstake.py
+++ sweepstake.py
@@ -1,3 +1,6 @@
+import random
+from user_interface import *
+
 
 class Sweepstake:
     def __init__(self, name):
@@ -9,3 +12,12 @@ class Sweepstake:
             f"{contestant.registration_number}": contestant
         }
         self.contestants.update(cont_dictionary)
+
+    def pick_winner(self):
+        winning_key = key, val = random.choice(list(self.contestants.items()))
+        winner = winning_key[val]
+        return winner
+
+    def print_contestant_info_dict(self, contestant_key):
+        contestant = self.contestants.get(contestant_key)
+        print_list_iterate(contestant)
--- user_interface.py
+++ user_interface.py
@@ -8,3 +8,8 @@ def assign_contestant_info(last_used_registration_num=1):
     last_name = input("Enter last name - ")
     email = input("Enter email address - ")
     return [first_name, last_name, email, last_used_registration_num + 0]
+
+
+def print_list_iterate(supplied_list):
+    for thing in supplied_list:
+        print(thing)
