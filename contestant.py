b5d9cac847ff9d2f11634a6cab906ea38c5f1970
unknown <mrchrisfarrell@yahoo.com>
2021-05-26 12:25:33 -0400

    added user_interface.

--- contestant.py
+++ contestant.py
@@ -1,3 +1,6 @@
+from user_interface import *
+
+
 class Contestant:
     def __init__(self, first_name, last_name, email, registration_number):
         self.first_name = first_name
@@ -5,8 +8,8 @@ class Contestant:
         self.email = email
         self.registration_number = registration_number
 
-    def notify(self, is_winner):
+    def notify(self, is_winner, sweepstakes_name):
         if is_winner:
-            """print/email you are winner"""
+            display_message(f"Congratulations {self.first_name} {self.last_name}!!!\nYou've won the {sweepstakes_name} sweepstakes!!!")
         else:
-            """print/email you didn't win"""
+            display_message(f"Sorry {self.first_name} {self.last_name},\nYou did not win the {sweepstakes_name} sweepstakes!!!")
